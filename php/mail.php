aa95f46b54c287318e17eccb1ebc7575d350183c
peppcy <90799500+peppcy@users.noreply.github.com>
2022-07-24 23:15:20 +0100

    mail php

--- php/mail.php
+++ php/mail.php
@@ -1,27 +1,125 @@
 <?php
-// Get data from form
-$name = $_POST['name'];
-$email= $_POST['email'];
-$tel= $_POST['tel'];
-$message= $_POST['message'];
-
-$to = "contact@ngosobo.info";
-$subject = "This is the subject line";
-
-// The following text will be sent
-// Name = user entered name
-// Email = user entered email
-// Message = user entered message
-$txt ="Name = ". $name . "\r\n Email = "
-	. $email . "\r\n Phone Number="$tel". " "\r\n  Message =" . $message;
-
-$headers = "From: noreply@ngosobo.info" . "\r\n" .
-			"CC: somebodyelse@example.com";
-if($email != NULL) {
-	mail($to, $subject, $txt, $headers);
+  
+if($_POST) {
+    $visitor_name = "";
+    $visitor_email = "";
+    $email_title = "";
+    $concerned_department = "";
+    $visitor_message = "";
+    $email_body = "<div>";
+      
+    if(isset($_POST['visitor_name'])) {
+        $visitor_name = filter_var($_POST['visitor_name'], FILTER_SANITIZE_STRING);
+        $email_body .= "<div>
+                           <label><b>Visitor Name:</b></label>&nbsp;<span>".$visitor_name."</span>
+                        </div>";
+    }
+ 
+    if(isset($_POST['visitor_email'])) {
+        $visitor_email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['visitor_email']);
+        $visitor_email = filter_var($visitor_email, FILTER_VALIDATE_EMAIL);
+        $email_body .= "<div>
+                           <label><b>Visitor Email:</b></label>&nbsp;<span>".$visitor_email."</span>
+                        </div>";
+    }
+      
+    if(isset($_POST['email_title'])) {
+        $email_title = filter_var($_POST['email_title'], FILTER_SANITIZE_STRING);
+        $email_body .= "<div>
+                           <label><b>Reason For Contacting Us:</b></label>&nbsp;<span>".$email_title."</span>
+                        </div>";
+    }
+      
+    if(isset($_POST['concerned_department'])) {
+        $concerned_department = filter_var($_POST['concerned_department'], FILTER_SANITIZE_STRING);
+        $email_body .= "<div>
+                           <label><b>Concerned Department:</b></label>&nbsp;<span>".$concerned_department."</span>
+                        </div>";
+    }
+      
+    if(isset($_POST['visitor_message'])) {
+        $visitor_message = htmlspecialchars($_POST['visitor_message']);
+        $email_body .= "<div>
+                           <label><b>Visitor Message:</b></label>
+                           <div>".$visitor_message."</div>
+                        </div>";
+    }
+      
+    if($concerned_department == "Ghana") {
+        $recipient = "contact@ngosobo.info";
+    }
+    else if($concerned_department == "Mali") {
+        $recipient = "contact@ngosobo.info";
+    }
+    else if($concerned_department == "Zimbabwe") {
+        $recipient = "contact@ngosobo.info";
+    }
+	if($concerned_department == "Cameroon") {
+        $recipient = "contact@ngosobo.info";
+    }
+    else if($concerned_department == "Niger Republic") {
+        $recipient = "contact@ngosobo.info";
+    }
+    else if($concerned_department == "Senegal") {
+        $recipient = "contact@ngosobo.info";
+    }
+	if($concerned_department == "Senegal") {
+        $recipient = "contact@ngosobo.info";
+    }
+    else if($concerned_department == "Uganda") {
+        $recipient = "contact@ngosobo.info";
+    }
+    else if($concerned_department == "Malawi") {
+        $recipient = "contact@ngosobo.info";
+    }
+	if($concerned_department == "Kenya") {
+        $recipient = "contact@ngosobo.info";
+    }
+    else if($concerned_department == "Ethiopia") {
+        $recipient = "contact@ngosobo.info";
+    }
+    else if($concerned_department == "South Africa") {
+        $recipient = "contact@ngosobo.info";
+    }
+	if($concerned_department == "Sudan") {
+        $recipient = "contact@ngosobo.info";
+    }
+    else if($concerned_department == "USA") {
+        $recipient = "contact@ngosobo.info";
+    }
+    else if($concerned_department == "") {
+        $recipient = "contact@ngosobo.info";
+    }
+	if($concerned_department == "Australia") {
+        $recipient = "contact@ngosobo.info";
+    }
+    else if($concerned_department == "UK") {
+        $recipient = "contact@ngosobo.info";
+    }
+    else if($concerned_department == "Canada") {
+        $recipient = "contact@ngosobo.info";
+    }
+	if($concerned_department == "Germany") {
+        $recipient = "contact@ngosobo.info";
+    }
+    
+    else {
+        $recipient = "contact@domain.com";
+    }
+     
+    $email_body .= "</div>";
+ 
+    $headers  = 'MIME-Version: 1.0' . "\r\n"
+    .'Content-type: text/html; charset=utf-8' . "\r\n"
+    .'From: ' . $visitor_email . "\r\n";
+      
+    if(mail($recipient, $email_title, $email_body, $headers)) {
+        echo "<p>Thank you for contacting us, $visitor_name. You will get a reply within 24 hours.</p>";
+    } else {
+        echo '<p>We are sorry but the email did not go through.</p>';
+    }
+      
+} else {
+    echo '<p>Something went wrong</p>';
 }
-
-// Redirect to
-header("Location:email.html");
 ?>
-
